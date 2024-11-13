7418a547b21990b0dee4a49f522fc352349532ad
LuizChristani <166182040+LuizChristani@users.noreply.github.com>
2024-11-13 12:54:06 -0300

    Update Footer.tsx

--- src/Pages/Login/Footer.tsx
+++ src/Pages/Login/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     return (
         <footer className="bg-gray-800 text-white p-4 w-full">
             <div className="container mx-auto text-center">
-                <p>&copy; {new Date().getFullYear()} SunBridge. Todos os direitos reservados.</p>
+                <p>&copy; {new Date().getFullYear()} Todos os direitos reservados.</p>
             </div>
         </footer>
     );
