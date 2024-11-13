fc4ae3642beb03b78c4a0184d6ef3df8c8a3005a
LuizChristani <166182040+LuizChristani@users.noreply.github.com>
2024-11-13 12:54:39 -0300

    Update Footer.tsx

--- src/Pages/Login/Footer.tsx
+++ src/Pages/Login/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 const Footer = () => {
     return (
-        <footer className="bg-gray-800 text-white p-4 w-full">
+        <footer className="bg-[#5599BE] text-white p-4 w-full">
             <div className="container mx-auto text-center">
                 <p>&copy; {new Date().getFullYear()} Todos os direitos reservados.</p>
             </div>
